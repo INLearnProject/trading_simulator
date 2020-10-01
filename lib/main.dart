69197e4736974066aca6c2aedb68175b313c04a8
BRICE DAVID BONGOUADE <bongouade@gmail.com>
2020-10-01 09:42:46 +0000

    RESET THE PROJECT

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-
-import 'package:trading_simulator/pages/splash/splash.dart';
-import 'app_localizations.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Trainning Flutter',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-
-      // List all of the app's supported locales here
-      supportedLocales: [
-        Locale('en', 'US'),
-        Locale('fr', 'FR'),
-      ],
-      // These delegates make sure that the localization data for the proper language is loaded
-      localizationsDelegates: [
-        // A class which loads the translations from JSON files
-        AppLocalizations.delegate,
-        // Built-in localization of basic text for Material widgets
-        GlobalMaterialLocalizations.delegate,
-        // Built-in localization for text direction LTR/RTL
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      // Returns a locale which will be used by the app
-      localeResolutionCallback: (locale, supportedLocales) {
-        // Check if the current device locale is supported
-        for (var supportedLocale in supportedLocales) {
-          if (supportedLocale.languageCode == locale.languageCode &&
-              supportedLocale.countryCode == locale.countryCode) {
-            return supportedLocale;
-          }
-        }
-        // If the locale of the device is not supported, use the first one
-        // from the list (English, in this case).
-        return supportedLocales.first;
-      },
-
-      home: Splash(),
-      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
 
-// import 'package:trading_simulator/app_localizations.dart';
-// AppLocalizations.of(context).translate('first_string')
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Reset Project"),
+      ),
+    );
+  }
+}
